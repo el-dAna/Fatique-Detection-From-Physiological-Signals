6c58cd8a5d13b24415dffef66e1c0db98a068155
el-dAna <100404513+el-dAna@users.noreply.github.com>
2023-11-28 11:11:32 +0000

    streamlit import error eventhough installed in activated env

--- Makefile
+++ Makefile
@@ -3,18 +3,18 @@ install:
 	pip install --upgrade pip && \
 		pip install -r requirements.txt
 
-
 test:
 	echo "TESTING CODE"
 	python -m pytest -vv --cov=cli --cov=mlib --cov=utilscli --cov=app test_mlib.py
 
 format:
 	echo "FORMATING CODE"
-	black *.py utils
+	black *.py ./mylib/appfunctions.py
 
 lint:
 	echo "LINTING CODE"
-	pylint --disable=R,C,W1203,E1101 utils #cli utilscli
+	pylint --disable=R,C,W1203,E1101 app.py
+	#pylint --disable=R,C,W1203,E1101 utils #cli #utilscli
 	#python -m pytest -vv test_main.py
 	#pylint --disable=R,C,W1203,E1101 mlib cli utilscli
 	#lint Dockerfile
--- mylib/appfunctions.py
+++ mylib/appfunctions.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import os
 import pandas as pd
+import boto3
+from io import BytesIO
 
 
 def upload_files():
@@ -10,13 +12,17 @@ def upload_files():
     Returns:
     - list or None: List of uploaded files or None if no files are uploaded.
     """
-    uploaded_files = st.file_uploader("Upload files", type="csv", accept_multiple_files=True)
+    uploaded_files = st.file_uploader(
+        "Upload files", type="csv", accept_multiple_files=True
+    )
 
     if uploaded_files:
         st.write("Uploaded files:")
         file_names = [file.name for file in uploaded_files]
         st.write("\n".join(file_names))
-        file_paths = [os.path.join(tempfile.gettempdir(), file.name) for file in uploaded_files]
+        file_paths = [
+            os.path.join(tempfile.gettempdir(), file.name) for file in uploaded_files
+        ]
 
         # file_name = uploaded_file.name
         # file_path = os.path.join(tempfile.gettempdir(), file_name)
@@ -29,7 +35,6 @@ def upload_files():
         return file_paths
 
     return None
-
 
 
 def read_files(file_paths):
@@ -50,4 +55,38 @@ def read_files(file_paths):
         st.write(data.describe())
     except Exception as e:
         st.error(f"Error reading file {selected_file}: {e}")
+        return None
+
+    # def load_data_from_s3_bucket(bucket_name, object_key, s3_client=boto3.client('s3')):
+    #     response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
+    #     #data = pd.read_csv(response['Body'])
+
+    #     return type(data)
+
+
+# AWS credentials (replace with your own credentials)
+aws_access_key_id = "your_access_key_id"
+aws_secret_access_key = "your_secret_access_key"
+aws_region = "your_region"
+
+# S3 bucket and file details
+bucket_name = "physiologicalsignals"
+file_key = "HealthySubjectsBiosignalsDataSet/Subject1/Subject1AccTempEDA.csv"
+
+
+# Function to load data from S3
+def load_data_from_s3():
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        region_name=aws_region,
+    )
+    try:
+        # Download the file from S3
+        obj = s3.get_object(Bucket=bucket_name, Key=file_key)
+        # data = pd.read_csv(BytesIO(obj['Body'].read()))
+        return obj
+    except Exception as e:
+        st.error(f"Error loading data from S3: {str(e)}")
         return None
