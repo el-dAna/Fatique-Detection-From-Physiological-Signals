0c2883eb829bf9e9ed6f3a0e51b15dc5a59fd933
el-dAna <100404513+el-dAna@users.noreply.github.com>
2023-12-27 12:26:46 +0000

    more progress on fstapi but incomplete

--- fast_api.py
+++ fast_api.py
@@ -1,25 +1,53 @@
-
-import uvicorn
+from typing import Union
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
-from mylib.apifunctions import get_experiments
 
-class User_input(BaseModel):
-    project_name: str
+from mylib.apifunctions import get_experiments, delete_experiments
 
 app = FastAPI()
 
-app.get("/")
-def index():
-    return {"name":"Ben"}
 
-# app.post('/experiments')
-# def get_experiment_list(input: User_input):
-#     experiment_list = get_experiments(project_name=input.project_name)
-#     return experiment_list
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+class User_train(BaseModel):
+    train_percent: float
+    sampling_window: int
+    degree_of_overlap: float
+    s3_model_name: str
+    clearml_task_name: str
+    epochs: int
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=9000)
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+
+
+@app.get("/exp/{project_name}/{task_name}")
+def get_experiment_list(project_name: str, task_name:str):
+    return get_experiments(project_name=project_name, task_name=task_name)
+
+
+@app.delete("/exp/{project_name}/")
+def delete_experiment_list(task_name:str, experiments_to_delete:list, project_name=str,):
+    return delete_experiments(task_name='user delete', experiments_to_delete=['test_ui'])
+
+
+
+# @app.post("/explist")
+# def train_model(input: User_train):
+#     #result = get_experiments(project_name=input.project_name, task_name=input.task_name)
+#     return result
--- mylib/apifunctions.py
+++ mylib/apifunctions.py
@@ -1,11 +1,26 @@
 from clearml import Task
 
-def get_experiments(project_name = 'portfolioproject'):
-    task = Task.init(project_name=project_name)
+def get_experiments(project_name='portfolioproject', task_name='testing api'):
+    task = Task.init(project_name=project_name,  task_name=task_name)
     tasks = Task.get_tasks(project_name=project_name)
     experiment_names = [task.name for task in tasks]
     task.close()
     return experiment_names
 
+def delete_experiments(experiments_to_delete, project_name='portfolioproject', task_name="deleteapitest"):
+    task = Task.init(project_name=project_name, task_name=task_name)
+    tasks = Task.get_tasks(project_name=project_name)
+    status = []
+    for task in tasks:
+        if task.id in experiments_to_delete:
+            print(task.id)
+            # try:
+            #     task.delete()
+            #     status.append(200)
+            # except Exception as e:
+            #     status.append(404)
+            #     continue
+    task.close()
+    return status
 
-# print(get_experiments())
+print(delete_experiments(experiments_to_delete=['test_ui']))
